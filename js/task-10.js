3ec48b98248d1633266992e792b4b43a7c640946
KovalVladyslav <vladxnos@gmail.com>
2023-03-06 00:12:37 +0200

    10th task
    done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,27 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = input.value;
+  let newArr = [];
+  for (let i = 0; i < input.value; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + 10 * i}px`;
+    box.style.height = `${30 + 10 * i}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    newArr.push(box);
+  }
+  boxes.append(...newArr);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
